1967f1978355324f6d12656746b6cf42f68458bd
tohshige <oshige@mono-96.jp>
2017-12-28 23:21:31 +0900

    dummy key file

---
+++ cred2.dist.js
@@ -0,0 +1,27 @@
+/* this is sample */
+module.exports = {
+  /* test account */
+  admin:{
+    name: 'hogehoge',
+    word: 'hogepass',
+  },
+  /* test account */
+  debug:{
+    shop_bid: '320124',
+    username: 'testxxxxxx',
+    password: 'youpass',
+
+    username2: 'dummmyyyyy',
+    password2: 'dummmyyyyy'
+  },
+  /* test account */
+  test:{
+    shop_bid: '320124',
+    username: 'testxxxxxx',
+    password: 'youpass',
+
+    username2: 'dummmyyyyy',
+    password2: 'dummmyyyyy'
+  }
+}
+
