2924676fa041c6c78465954506fc29edb2cd6b7e
tohshige <oshige@mono-96.jp>
2017-12-29 23:46:02 +0900

    up

--- exp/app.js
+++ exp/app.js
@@ -35,8 +35,7 @@ app.use(session({
 	resave: false,
     saveUninitialized: true,
 	cookie: {
-		// maxAge: 5* 24 * 60 * 60 * 1000
-		maxAge:  3*60*1000
+		maxAge: 5* 24 * 60 * 60 * 1000
 	}
 }));
 app.use('/', index);
--- exp/routes/login.js
+++ exp/routes/login.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const $ = require('jquery');
 const router = express.Router();
+let CREDS1 = require('../../cred2');// load IPASS
 
 router.get('/', function(req, res, next) {
 	res.render('login');
 });
 
 router.post('/', function(req, res, next) {
-	if (req.body.userName ==='admin' && req.body.passWord ==='admin') {
+	if (req.body.userName ===CREDS1.admin.name && req.body.passWord ===CREDS1.admin.word) {
 		req.session.user = { name: req.body.userName };
 		res.redirect('../');
 	} else {
